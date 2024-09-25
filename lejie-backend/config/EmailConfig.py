160222d99d32aeca69b4143af84832f86cf63dba
miyagipipi <liqisheng2016@sina.com>
2024-09-25 15:50:14 +0800

    Update EmailConfig.py

--- lejie-backend/config/EmailConfig.py
+++ lejie-backend/config/EmailConfig.py
@@ -1,6 +1,6 @@
-FORM = 'liqisheng2016@sina.com'
+FORM = 'YOUR EMAIL'
 SUBJECT = '乐接-API 接口开放平台-验证码邮件'
 EMAIL_HTML_CONTENT_PATH = "email.html"
-PASSWORD = 'fe7bcf3b9c510500'
+PASSWORD = 'YOUR PASSWORD'
 HOST='smtp.sina.com'
 EMAIL_REGEX = "^[A-Za-z0-9]+[A-Za-z0-9._-]*@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"
